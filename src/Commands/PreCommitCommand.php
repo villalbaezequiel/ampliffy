4fa1bc19b43db22cdae2ef2861dd6824ca29e3a1
Ezequiel Villalba <villalbaezequiel64@gmail.com>
2021-10-18 02:30:03 -0300

    set data in changes commits

--- src/Commands/PreCommitCommand.php
+++ src/Commands/PreCommitCommand.php
@@ -6,6 +6,9 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use App\Models\Repository;
+use App\Models\Branch;
+use App\Models\Commit;
 
 class PreCommitCommand extends Command
 {
@@ -16,6 +19,7 @@ class PreCommitCommand extends Command
 
         $this
             ->addArgument('repository', InputArgument::REQUIRED, '')
+            ->addArgument('repository-origin', InputArgument::REQUIRED, '')
             ->addArgument('branch', InputArgument::REQUIRED, '')
             ->addArgument('hash-commit', InputArgument::REQUIRED, '')
             ->addArgument('description-commit', InputArgument::REQUIRED, '')
@@ -27,8 +31,45 @@ class PreCommitCommand extends Command
     {
         try {
             $output->writeLn("Observing and Saving changes pre-commit...");
+
+            // validate isset Repo
+            $checkRepository = Repository::where('name', $input->getArgument('repository'))
+                                        ->where('remote_url', $input->getArgument('repository-origin'))
+                                        ->first();
+
+            if (!$checkRepository) {
+                // new Repository
+                $repository = new Repository;
+                $repository->name = $input->getArgument('repository');
+                $repository->remote_url = $input->getArgument('repository-origin');
+                $repository->save();
+            }
+
+            // validate isset Repo with relation Branch
+            $checkBranch = Branch::where('name', $input->getArgument('branch'))
+                                ->where('id_repository', '!=', $repository->id)
+                                ->first();
+
+            if (!$checkBranch) {
+                // new Branch
+                $branch = new Branch;
+                $branch->name = $input->getArgument('branch');
+                $branch->id_repository = $repository->id;
+                $branch->save();
+            }
+
+            // new Commit
+            $commit = new Commit;
+            $commit->id_branch = $branch->id;
+            $commit->hash = $input->getArgument('hash-commit');
+            $commit->description = $input->getArgument('description-commit');
+            $commit->author = $input->getArgument('author-commit');
+            $commit->date = $input->getArgument('date-commit');
+            $commit->save();
+
             return Command::SUCCESS;
         } catch (\Exception $err) {
+            var_dump($err);
         }
     }
 }
