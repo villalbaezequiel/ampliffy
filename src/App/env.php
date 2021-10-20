975e880d6ca21b50e46cb4c1e41c0e6d9bf7e9f4
Ezequiel Villalba <villalbaezequiel64@gmail.com>
2021-10-20 16:16:26 -0300

    add configs for commands

---
+++ src/App/env.php
@@ -0,0 +1,12 @@
+<?php
+
+$db = [
+    'driver'    => 'mysql',
+    'host'      => 'mysql',
+    'database'  => 'challenge',
+    'username'  => 'user',
+    'password'  => 'root',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+];
--- src/bin/console.php
+++ src/bin/console.php
@@ -1,11 +1,14 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/Configs.php';
 
 use App\Commands\CommitMsgCommand;
 use App\Commands\PostCommitCommand;
 use Symfony\Component\Console\Application;
+use App\Services\ConnectionService;
 
+$initConnection = new ConnectionService($db);
 $commitMsgCommand = new CommitMsgCommand();
 $postCommitCommand = new PostCommitCommand();
 
