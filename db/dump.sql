47e3e32cbe279a5e05d215d9eeb5a6729495ae03
Ezequiel Villalba <villalbaezequiel64@gmail.com>
2021-10-20 19:14:37 -0300

    upgrade schema db, add relationship tables

--- db/dump.sql
+++ db/dump.sql
@@ -1,24 +1,25 @@
 CREATE TABLE `repositories` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
   `remote_url` varchar(255) NOT NULL DEFAULT '',
   `is_home` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY ( id )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `branches` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `id_repository` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_repository` int(4) NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY ( id ),
+  FOREIGN KEY (id_repository) REFERENCES repositories(id) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `commits` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_branch` int(11) NOT NULL,
   `hash` varchar(255) NULL DEFAULT '',
   `description` varchar(500) NOT NULL DEFAULT '',
@@ -27,5 +28,7 @@ CREATE TABLE `commits` (
   `date` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY ( id ),
+  FOREIGN KEY (id_branch) REFERENCES branches(id) 
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
